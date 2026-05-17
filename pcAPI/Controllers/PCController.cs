594a304f99f3011e77504f4c5269adeb941efeda
Yuuki <s33994@pjwstk.edu.pl>
2026-05-17 22:40:21 +0200

    Added controller

---
+++ pcAPI/Controllers/PCController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pcAPI.DTOs;
+using pcAPI.Exceptions;
+using pcAPI.Service;
+
+namespace pcAPI.Controllers;
+
+[ApiController]
+[Route("api/pcs")]
+public class PCController(IPcService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        return Ok(await service.GetAllAsync(cancellationToken));
+    }
+
+    [HttpGet("{id:int}/components")]
+    public async Task<IActionResult> GetById(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return Ok(await service.GetByIdAsync(id, cancellationToken));
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(CreatePCDTO request, CancellationToken cancellationToken)
+    {
+        var pc = await service.AddAsync(request, cancellationToken);
+        return CreatedAtAction(nameof(GetById), new { id = pc.Id }, pc);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, UpdatePCDTO request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await service.UpdateAsync(id, request, cancellationToken);
+            return NoContent();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await service.DeleteAsync(id, cancellationToken);
+            return NoContent();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+}
